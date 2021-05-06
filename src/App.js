9e46ceaa37002e5c935fe8212d9b7ba6b4fd4401
RavitBouclairIL <rdabush@bouclair.com>
2021-05-06 11:46:41 +0300

    run prettier

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 import store from './redux/store';
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <div className="app">
-          <h1>Setup React Redux</h1>
-      </div>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <div className="app">
+                <h1>Setup React Redux</h1>
+            </div>
+        </Provider>
+    );
 };
 
 export default App;
